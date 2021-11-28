f1949ae8cd934b33ad36c95f12b15fbf654c6955
SayanaZhamtsaranova <sayana_0308@mail.ru>
2021-11-28 16:08:48 +0900

    second commit

--- app/src/main/java/com/sayana/mysecondapp/MainActivity.kt
+++ app/src/main/java/com/sayana/mysecondapp/MainActivity.kt
@@ -6,41 +6,50 @@ import android.util.Log
 
 const val TAG="MainActivity"
 
+
+//Сегодня солнышко печёт.
+//Сегодня праздник — Май.
+//Влезай, сынишка, на плечо
+//И флаг свой поднимай!
+//А через год пойдёшь пешком
+//С красным праздничным флажком.
+
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d(TAG, "onCreate")
+        Log.d(TAG, "Сегодня солнышко печёт.")
     }
 
     override fun onStart() {
         super.onStart()
-        Log.d(TAG, "onStart")
+        Log.d(TAG, "Сегодня праздник — Май.")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.d(TAG, "onResume")
+        Log.d(TAG, "Влезай, сынишка, на плечо")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.d(TAG, "onPause")
+        Log.d(TAG, "И флаг свой поднимай!")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.d(TAG, "onStop")
+        Log.d(TAG, "А через год пойдёшь пешком")
     }
 
     override fun onRestart() {
         super.onRestart()
-        Log.d(TAG, "onRestart")
+        Log.d(TAG, "RESTART")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.d(TAG, "onDestroy")
+        Log.d(TAG, "С красным праздничным флажком.")
     }
 }
